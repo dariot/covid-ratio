19cc0518f69d99405cab8a06b304cce2198d164b
dariot <dario.turchetto@gmail.com>
2022-03-05 09:30:21 +0100

    removed index page update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,5 @@
 class HomeController < ApplicationController
   def index
     @reading = Reading.last
-    current_hour = Time.now.hour
-    current_day_of_year = Time.now.yday
-
-    if current_day_of_year == @reading.date.yday + 1 and current_hour >= 17
-      Reading.update_db
-      @reading = Reading.last
-    end
   end
 end
